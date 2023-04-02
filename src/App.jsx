5a6e81dbf1f899dea7c6f077529eddfffe12c60b
ShamimHossain1 <beastb697@gmail.com>
2023-04-03 00:30:27 +0600

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      
+    <div className="App text-center mt-5">
+         <h1 className='bg-danger text-white py-5'>Hello Bootstrap</h1>
     </div>
   )
 }
